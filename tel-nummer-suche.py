a56fb1e865bc1cd6429d63f654a28ba6dbb111fe
Nikolaj Veljkovic <nikolaj.veljkovic@mygymer.ch>
2024-02-23 13:46:35 +0100

    User friendly version

--- tel-nummer-suche.py
+++ tel-nummer-suche.py
@@ -5,16 +5,18 @@ search.ch nach den entsprechenden Telefonnummern
 from urllib import request, parse
 from re import findall
 
+def suche(was, wo):
+    uf = request.urlopen(url)
+    html = str(uf.read())
+    resultat = findall(pattern, html)
+    return resultat[0]
+    
 print("Bitte verwende keine umlaute, sondern schreibe sie als (ü -> ue)")
 user_was = input("Was suchst du? ")
 user_wo = input("Wo suchst du es? ")
 
 url = "https://search.ch/tel/?was=" + user_was + "&wo=" + user_wo
+pattern = "title=\"Anrufen\">([0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2})[\s*]?"
+telefon_nummer = suche(user_was, user_wo)
 
-uf = request.urlopen(url)
-html = str(uf.read())
-print(html)
-
-liste = findall('<ul class="tel-result-actions sl-screenonly sl-floatlist"><li><a class="tel-result-action sl-icon-call" href="tel:+[\d]', html)
-
-print(liste)
+print(f"Du kannst hier anrufen: {telefon_nummer}")
